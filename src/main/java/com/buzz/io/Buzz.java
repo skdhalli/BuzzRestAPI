f77cf62c14ac0a21a72658329d5c2c4e59051969
Sai Dhalli <c_said@qti.qualcomm.com>
2014-12-23 01:42:14 -0800

    -a

---
+++ src/main/java/com/buzz/io/Buzz.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.buzz.io;
+
+
+import com.buzz.buzzdata.DistanceUnits;
+import com.buzz.buzzdata.IBuzzDB;
+import com.buzz.buzzdata.MongoBuzz;
+import com.mongodb.MongoException;
+import java.net.UnknownHostException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/**
+ * REST Web Service
+ *
+ * @author sdhalli
+ */
+@Path("buzz")
+public class Buzz {
+
+    @Context
+    private UriInfo context;
+    
+    IBuzzDB buzzDB;
+ 
+    /**
+     * Creates a new instance of Buzz
+     * @throws java.net.UnknownHostException
+     */
+    public Buzz() throws UnknownHostException {
+       buzzDB = new MongoBuzz("162.219.245.33", 27017, "Buzz", "admin", "qNjneHcKyl");
+    }
+
+    /**
+     * Retrieves representation of an instance of BuzzIO.BuzzService
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/json")
+    public String getJson() {
+        //TODO return proper representation object
+        return "Welcome to Buzz Services";
+    }
+    
+      @GET
+    @Path("Add")
+    @Produces("application/json")
+    public String AddBuzz(@QueryParam("userid") String userid, 
+            @QueryParam("header") String header, 
+            @QueryParam("content") String content,
+            @QueryParam("lat") Double lat, 
+            @QueryParam("lng") Double lng, 
+            @QueryParam("tags") String tags) 
+            throws MongoException, UnknownHostException
+    {
+        String retval = "Success";
+        buzzDB.Insert(userid, header, content, lat, lng, tags);
+        return retval;
+    }
+    
+    @GET
+    @Path("GeoSearch")
+    @Produces("application/json")
+    public String SearchBuzz(@QueryParam("lat") Double lat, 
+            @QueryParam("lng") Double lng,
+            @QueryParam("distance") double distance, 
+            @QueryParam("units") String units,
+            @QueryParam("tags") String tags) 
+            throws MongoException, UnknownHostException
+    {
+        String retval ="";
+        DistanceUnits units_api = DistanceUnits.miles;
+        switch(units.toUpperCase())
+        {
+            case "MILES":
+                units_api = DistanceUnits.miles;
+                break;
+            case "KILOMETERS":
+                units_api = DistanceUnits.kilometers;
+                break;
+        }
+        retval = buzzDB.SearchByLocation(lat, lng, distance, units_api, tags);
+        return retval;
+    }
+}
